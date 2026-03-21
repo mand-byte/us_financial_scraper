cf90f0e090fa485f09ef71efcbdf88ce1cacd9af
hubber <jony83@gmail.com>
2026-03-21 12:45:45 +0800

    Style: Reformat `_EXPORT_MAP` entries in `src/model/__init__.py` for improved readability and delete the `uv.lock` file.

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,35 +2,78 @@ from importlib import import_module
 
 _EXPORT_MAP = {
     "BaseClickHouseModel": ("src.model.base_clickhouse_model", "BaseClickHouseModel"),
-    "BenchmarkEtfKlineModel": ("src.model.us_benchmark_etf_kline_model", "BenchmarkEtfKlineModel"),
+    "BenchmarkEtfKlineModel": (
+        "src.model.us_benchmark_etf_kline_model",
+        "BenchmarkEtfKlineModel",
+    ),
     "SecForm10KModel": ("src.model.sec_form10k_model", "SecForm10KModel"),
     "SecForm10QModel": ("src.model.sec_form10q_model", "SecForm10QModel"),
     "SecForm13FModel": ("src.model.sec_form13f_model", "SecForm13FModel"),
     "SecForm4Model": ("src.model.sec_form4_model", "SecForm4Model"),
     "SecForm8KModel": ("src.model.sec_form8k_model", "SecForm8KModel"),
     "SecSC13DModel": ("src.model.sec_sc13d_model", "SecSC13DModel"),
-    "UsBenchmarkEtfKlineModel": ("src.model.us_benchmark_etf_kline_model", "BenchmarkEtfKlineModel"),
-    "GdeltMacroSentimentModel": ("src.model.gdelt_macro_sentiment_model", "GdeltMacroSentimentModel"),
-    "UsMacroDailyKlineModel": ("src.model.us_macro_daily_kline_model", "UsMacroDailyKlineModel"),
-    "UsMacroIndicatorsModel": ("src.model.us_macro_indicators_model", "UsMacroIndicatorsModel"),
-    "UsStock10kSectionsRawModel": ("src.model.us_stock_10k_sections_raw_model", "UsStock10kSectionsRawModel"),
-    "UsStockBalanceSheetsModel": ("src.model.us_stock_balance_sheets_model", "UsStockBalanceSheetsModel"),
-    "UsStockCashFlowStatementsModel": ("src.model.us_stock_cash_flow_statements_model", "UsStockCashFlowStatementsModel"),
-    "UsStockDividendsModel": ("src.model.us_stock_dividends_model", "UsStockDividendsModel"),
-    "UsStockFigiTickerMappingModel": ("src.model.us_stock_figi_ticker_mapping_model", "UsStockFigiTickerMappingModel"),
-    "UsStockIncomeStatementsModel": ("src.model.us_stock_income_statements_model", "UsStockIncomeStatementsModel"),
-    "UsStockMinutesKlineModel": ("src.model.us_stock_minutes_kline_model", "UsStockMinutesKlineModel"),
-    "UsStockMinutesKlineStateModel": ("src.model.us_stock_minutes_kline_state_model", "UsStockMinutesKlineStateModel"),
+    "UsBenchmarkEtfKlineModel": (
+        "src.model.us_benchmark_etf_kline_model",
+        "BenchmarkEtfKlineModel",
+    ),
+    "GdeltMacroSentimentModel": (
+        "src.model.gdelt_macro_sentiment_model",
+        "GdeltMacroSentimentModel",
+    ),
+    "UsMacroDailyKlineModel": (
+        "src.model.us_macro_daily_kline_model",
+        "UsMacroDailyKlineModel",
+    ),
+    "UsMacroIndicatorsModel": (
+        "src.model.us_macro_indicators_model",
+        "UsMacroIndicatorsModel",
+    ),
+    "UsStock10kSectionsRawModel": (
+        "src.model.us_stock_10k_sections_raw_model",
+        "UsStock10kSectionsRawModel",
+    ),
+    "UsStockBalanceSheetsModel": (
+        "src.model.us_stock_balance_sheets_model",
+        "UsStockBalanceSheetsModel",
+    ),
+    "UsStockCashFlowStatementsModel": (
+        "src.model.us_stock_cash_flow_statements_model",
+        "UsStockCashFlowStatementsModel",
+    ),
+    "UsStockDividendsModel": (
+        "src.model.us_stock_dividends_model",
+        "UsStockDividendsModel",
+    ),
+    "UsStockFigiTickerMappingModel": (
+        "src.model.us_stock_figi_ticker_mapping_model",
+        "UsStockFigiTickerMappingModel",
+    ),
+    "UsStockIncomeStatementsModel": (
+        "src.model.us_stock_income_statements_model",
+        "UsStockIncomeStatementsModel",
+    ),
+    "UsStockMinutesKlineModel": (
+        "src.model.us_stock_minutes_kline_model",
+        "UsStockMinutesKlineModel",
+    ),
+    "UsStockMinutesKlineStateModel": (
+        "src.model.us_stock_minutes_kline_state_model",
+        "UsStockMinutesKlineStateModel",
+    ),
     "UsStockNewsRawModel": ("src.model.us_stock_news_raw_model", "UsStockNewsRawModel"),
-    "UsStockRiskFactorsModel": ("src.model.us_stock_risk_factors_model", "UsStockRiskFactorsModel"),
-    "UsStockRiskTaxonomyModel": ("src.model.us_stock_risk_taxonomy_model", "UsStockRiskTaxonomyModel"),
+    "UsStockRiskFactorsModel": (
+        "src.model.us_stock_risk_factors_model",
+        "UsStockRiskFactorsModel",
+    ),
+    "UsStockRiskTaxonomyModel": (
+        "src.model.us_stock_risk_taxonomy_model",
+        "UsStockRiskTaxonomyModel",
+    ),
     "UsStockSplitsModel": ("src.model.us_stock_splits_model", "UsStockSplitsModel"),
-    "UsStockUniverseModel": ("src.model.us_stock_universe_model", "UsStockUniverseModel"),
-    "SecForm10kModel": ("src.model.sec_form10k_model", "SecForm10KModel"),
-    "SecForm10qModel": ("src.model.sec_form10q_model", "SecForm10QModel"),
-    "SecForm13fModel": ("src.model.sec_form13f_model", "SecForm13FModel"),
-    "SecForm8kModel": ("src.model.sec_form8k_model", "SecForm8KModel"),
-    "SecSc13dModel": ("src.model.sec_sc13d_model", "SecSC13DModel"),
+    "UsStockUniverseModel": (
+        "src.model.us_stock_universe_model",
+        "UsStockUniverseModel",
+    ),
 }
 
 
